7aa99a0c122acee8c31760142a51b1608a273ad8
Lee Richmond <lee.richmond@shopify.com>
2022-06-30 12:18:52 -0400

    Update spec helper

--- lib/graphiti_gql/spec_helper.rb
+++ lib/graphiti_gql/spec_helper.rb
@@ -2,6 +2,21 @@ module GraphitiGql
   module SpecHelper
     extend ActiveSupport::Concern
 
+    module ScopeTrackable
+      def self.prepended(klass)
+        klass.class_eval do
+          class << self
+            attr_accessor :resolved_scope
+          end
+        end
+      end
+
+      def resolve(scope)
+        self.class.resolved_scope = scope
+        super
+      end
+    end
+
     included do
       extend Forwardable
       def_delegators :result,
@@ -11,29 +26,36 @@ module GraphitiGql
         :nodes,
         :stats
 
+      Graphiti::Resource.send(:prepend, ScopeTrackable)
+
       if defined?(RSpec)
-        let(:params) do
-          fields = []
-          resource.attributes.each_pair do |name, config|
-            (fields << name) if config[:readable]
-          end
-          { fields: fields }
-        end
+        let(:params) { {} }
         let(:resource) { described_class }
         let(:ctx) { {} }
+        let(:only_fields) { [] }
+        let(:except_fields) { [] }
 
         def self.only_fields(*fields)
-          before do
-            fields.each { |f| params[:fields].select! { |p| p == f } }
-          end
+          let(:only_fields) { fields }
         end
 
         def self.except_fields(*fields)
-          before do
-            fields.each { |f| params[:fields].reject! { |p| p == f } }
-          end
+          let(:except_fields) { fields }
         end
       end
+    end
+
+    def fields
+      fields = []
+      resource.attributes.each_pair do |name, config|
+        (fields << name) if config[:readable]
+      end
+      if respond_to?(:only_fields) && only_fields.present?
+        fields.select! { |f| only_fields.include?(f) }
+      elsif respond_to?(:except_fields) && except_fields.present?
+        fields.reject! { |f| except_fields.include?(f) }
+      end
+      fields
     end
 
     def gql_datetime(timestamp, precise = false)
@@ -44,9 +66,16 @@ module GraphitiGql
       end
     end
 
+    def proxy
+      resource.gql(params.merge(fields: fields), ctx)
+    end
+
+    def query
+      proxy.query
+    end
+
     def run
       lambda do
-        proxy = resource.gql(params, ctx)
         proxy.to_h
         proxy
       end
