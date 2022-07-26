34c3360a2ca5fbbe3681dd246b303f08be4d9a08
Lee Richmond <lee.richmond@shopify.com>
2022-07-26 12:14:28 -0400

    Refactor controller gql context

--- lib/graphiti_gql/engine.rb
+++ lib/graphiti_gql/engine.rb
@@ -9,33 +9,19 @@ module GraphitiGql
       GraphitiGql.schema!
     end
 
-    module ControllerContext
-      def graphql_context
-        ctx = { controller: self }
-        ctx[:current_user] = current_user if respond_to?(:current_user)
-        ctx
-      end
-    end
-
     initializer "graphiti_gql.define_controller" do
       app_controller = GraphitiGql.config.application_controller || ::ApplicationController
-      app_controller.send(:include, ControllerContext)
 
       # rubocop:disable Lint/ConstantDefinitionInBlock(Standard)
       class GraphitiGql::ExecutionController < app_controller
         def execute
           params = request.params # avoid strong_parameters
           variables = params[:variables] || {}
+          ctx = respond_to?(:graphql_context) ? graphql_context : {}
           result = GraphitiGql.run params[:query],
             params[:variables],
-            graphql_context
+            ctx
           render json: result
-        end
-
-        private
-
-        def default_context
-          defined?(:current_user)
         end
       end
     end
