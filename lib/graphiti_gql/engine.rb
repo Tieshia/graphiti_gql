e6fee56b023c42958238f536ebda01daffd7cb66
Charles C. Lee <charleschanlee@gmail.com>
2022-07-26 15:47:56 -0700

    Load execution controller using Rails autoloader

--- lib/graphiti_gql/engine.rb
+++ lib/graphiti_gql/engine.rb
@@ -3,27 +3,10 @@ module GraphitiGql
     isolate_namespace GraphitiGql
 
     # TODO improvable?
-    config.after_initialize do
+    config.to_prepare do
     # initializer "graphiti_gql.generate_schema" do
       Dir.glob("#{Rails.root}/app/resources/**/*").each { |f| require(f) }
       GraphitiGql.schema!
-    end
-
-    initializer "graphiti_gql.define_controller" do
-      app_controller = GraphitiGql.config.application_controller || ::ApplicationController
-
-      # rubocop:disable Lint/ConstantDefinitionInBlock(Standard)
-      class GraphitiGql::ExecutionController < app_controller
-        def execute
-          params = request.params # avoid strong_parameters
-          variables = params[:variables] || {}
-          ctx = respond_to?(:graphql_context) ? graphql_context : {}
-          result = GraphitiGql.run params[:query],
-            params[:variables],
-            ctx
-          render json: result
-        end
-      end
     end
   end
 end
